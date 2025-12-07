0a1b7ba3e44b5c581d562a7116ad4e5537d9ca18
JoannaLei21 <lovelilei21@gmail.com>
2025-12-07 11:55:47 +0800

    feat:add notosansTC fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Lusitana, Chokokutai, Noto_Sans_TC } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+  weight: ['400', '700'],
+  subsets: ['latin'] 
+});
+export const chokokutai = Chokokutai({
+  weight: ['400'],
+  subsets: ['latin'],   
+});
+export const notosansTC = Noto_Sans_TC({
+  weight: ['400'],
+
+});
